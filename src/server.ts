e94ff5ef879017a0062197191825a2ccff711633
shreyash1231 <chandwadkarshreyash@gmail.com>
2026-05-22 09:19:27 +0530

    updated server.js

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express from "express";
-import PaymentOrder from "./routes/payment.route.js"
-import AdminRoute from "./routes/admin.route.js"
+import PaymentOrder from "./routes/payment.route.js";
+import AdminRoute from "./routes/admin.route.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+
 dotenv.config();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
 connectDB();
+
 app.use(
   cors({
     origin: [
@@ -20,9 +24,12 @@ app.use(
     credentials: true,
   })
 );
+
+app.options("*", cors());
+
 app.use(express.json());
 
-app.use("/api/v1/", PaymentOrder);
+app.use("/api/v1", PaymentOrder);
 app.use("/api/v2/admin", AdminRoute);
 
 app.listen(PORT, () => {
